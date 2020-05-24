18290111407328655b2256f902132f4870b4738b
OddMagnet <mibruenen@gmail.com>
2020-05-24 20:58:06 +0200

    Reset ContentView to blank state

--- 016-024 Starting SwiftUI/Project 1/WeSplit/WeSplit/ContentView.swift
+++ 016-024 Starting SwiftUI/Project 1/WeSplit/WeSplit/ContentView.swift
@@ -9,22 +9,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Harry", "Hermione", "Ron"] // no need for @State since its a constant
-    @State private var selectedStudent = 0      // can change, so @State is needed
-
     var body: some View {
-        VStack {
-            // Labels are important for clarity, but for Screenreaders too
-            Picker("Select your student", selection: $selectedStudent) {
-                ForEach(0 ..< students.count) {
-                    // create a text view for each student
-                    Text(self.students[$0]) // two-way binding so it updates the selection
-                }
-            }
-            Text("You chose: Student # \(students[selectedStudent])")
-        }
+        Text("Hello World")
     }
 }
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
