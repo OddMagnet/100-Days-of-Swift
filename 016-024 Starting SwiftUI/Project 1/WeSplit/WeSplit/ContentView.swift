96ef6ec5fb0dbbb1764ddc9c096045251cef39c5
OddMagnet <mibruenen@gmail.com>
2020-05-25 10:12:04 +0200

    Add state properties and fields for user input

--- 016-024 Starting SwiftUI/Project 1/WeSplit/WeSplit/ContentView.swift
+++ 016-024 Starting SwiftUI/Project 1/WeSplit/WeSplit/ContentView.swift
@@ -9,8 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    // State properties
+    @State private var checkAmount = ""         // cost of the check
+    // Double would be better for numbers, but SwiftUI only allows strings in text views
+    @State private var numberOfPeople = 2       // how many are sharing the cost
+    @State private var tipAmount = 2            // how much of a tip will be left
+    
+    // Constants
+    let tipPercentages = [10, 15, 20, 25, 0]    // holds values for the tip picker
+    
     var body: some View {
-        Text("Hello World")
+        Form {
+            Section {
+                // TextField view with a placeholder text and two-way binding to the state property
+                TextField("Amount", text: $checkAmount)
+                    .keyboardType(.decimalPad)  // only show a decimal keypad to avoid non-digits
+            }
+            Section {
+                Text("$\(checkAmount)")
+            }
+        }
     }
 }
 
