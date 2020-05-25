b47f172952950d68dd1a5ec771446563576f00bb
OddMagnet <mibruenen@gmail.com>
2020-05-25 10:32:34 +0200

    Add section for tip percentage, add segmented picker, small fixes

--- 016-024 Starting SwiftUI/Project 1/WeSplit/WeSplit/ContentView.swift
+++ 016-024 Starting SwiftUI/Project 1/WeSplit/WeSplit/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     @State private var checkAmount = ""         // cost of the check
     // Double would be better for numbers, but SwiftUI only allows strings in text views
     @State private var numberOfPeople = 2       // how many are sharing the cost
-    @State private var tipAmount = 2            // how much of a tip will be left
+    @State private var tipPercentage = 2            // how much of a tip will be left
     
     // Constants
     let tipPercentages = [10, 15, 20, 25, 0]    // holds values for the tip picker
@@ -26,13 +26,26 @@ struct ContentView: View {
                     TextField("Amount", text: $checkAmount)
                         .keyboardType(.decimalPad)  // only show a decimal keypad to avoid non-digits
                     
-                    // Picker view to choose the number of people splitting the check
+                    // Picker view for the number of people splitting the check
                     Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2 ..< 20) {
+                        ForEach(2 ..< 21) {
+                            // create a row up to 20 people
                             Text("\($0) people")
                         }
                     }
                 }
+                
+                Section(header: Text("How much tip do you want to leave?")) {                    
+                    // Picker view for the tip percentage selection
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            // create a 'row' for every tip percentage option
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())    // best suited for fewer items
+                }
+                
                 Section {
                     Text("$\(checkAmount)")
                 }
