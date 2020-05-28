c237c6e60363678f1ad18e35749f8f87480af883
OddMagnet <mibruenen@gmail.com>
2020-05-28 12:49:58 +0200

    Initial commit + stacking views

---
+++ 016-024 Starting SwiftUI/Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Michael Brünen on 28.05.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("1")
+                Text("2")
+                Text("3")
+            }
+            HStack {
+                Text("4")
+                Text("5")
+                Text("6")
+            }
+            HStack {
+                Text("7")
+                Text("8")
+                Text("9")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
