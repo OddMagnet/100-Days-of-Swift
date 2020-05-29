7bf9281accf4fc8b2e97d43c03142b19676c4016
OddMagnet <mibruenen@gmail.com>
2020-05-29 16:26:13 +0200

    Make UI prettier

--- 016-024 Starting SwiftUI/Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ 016-024 Starting SwiftUI/Project 2/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -20,12 +20,14 @@ struct ContentView: View {
     
     var body: some View {
         ZStack {
-            Color.blue.edgesIgnoringSafeArea(.all)
+            LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             VStack(spacing: 30) {
                 VStack {
                     Text("Tap the flag of")
                         .foregroundColor(.white)
                     Text(countries[correctAnswer])
+                        .font(.largeTitle)
+                        .fontWeight(.black)
                         .foregroundColor(.white)
                 }
                 ForEach(0 ..< 3) { number in        // create flag images as buttons
@@ -34,6 +36,9 @@ struct ContentView: View {
                     }) {
                         Image(self.countries[number])
                             .renderingMode(.original)
+                            .clipShape(Capsule())
+                            .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+                            .shadow(color: .black, radius: 2)
                     }
                 }
                 Spacer()
