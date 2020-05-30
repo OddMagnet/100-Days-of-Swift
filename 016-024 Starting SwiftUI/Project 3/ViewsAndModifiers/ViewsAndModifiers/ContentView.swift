5dfec25e3cfd8aa5b4945fadb13ed70b1fb5e653
OddMagnet <mibruenen@gmail.com>
2020-05-30 07:16:02 +0200

    Add custom modifier example

--- 016-024 Starting SwiftUI/Project 3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ 016-024 Starting SwiftUI/Project 3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 
+// custom view example
 struct CapsuleText: View {
     var text: String
     
@@ -17,6 +18,27 @@ struct CapsuleText: View {
             .foregroundColor(.white)
             .background(Color.blue)
             .clipShape(Capsule())
+    }
+}
+
+// custom modifier example
+struct Watermark: ViewModifier {
+    var text: String
+    
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.black)
+        }
+    }
+}
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
     }
 }
 
@@ -66,7 +88,14 @@ struct ContentView: View {
             
             VStack {
                 // view composition example
-                CapsuleText(text: "View composition example")
+                CapsuleText(text: "Custom view example")
+            }
+            
+            VStack {
+                // custom view modifier example
+                Color.blue
+                    .frame(width: 200, height: 100)
+                    .watermarked(with: "Custom modifier example")
             }
         }
     }
