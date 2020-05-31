b3335095beb6ee1ace28bf85bb8c399f0eb73fc2
OddMagnet <mibruenen@gmail.com>
2020-05-31 06:04:27 +0200

    Initial commit

---
+++ 026-034 Expanding your skills/Project 4/BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Michael Brünen on 31.05.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
