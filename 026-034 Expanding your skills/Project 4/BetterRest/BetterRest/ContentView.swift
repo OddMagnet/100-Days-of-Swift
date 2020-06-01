a08b2feae991d8802107b723c35d141c55c2e829
OddMagnet <mibruenen@gmail.com>
2020-06-01 05:22:58 +0200

    Add basic UI layout

--- 026-034 Expanding your skills/Project 4/BetterRest/BetterRest/ContentView.swift
+++ 026-034 Expanding your skills/Project 4/BetterRest/BetterRest/ContentView.swift
@@ -9,9 +9,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    // state
+    @State private var wakeUp = Date()
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
     
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .font(.headline)
+
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+                    Text("\(sleepAmount, specifier: "%g") hours")
+                }
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper(value: $coffeeAmount, in: 1...20) {
+                    Text("\(coffeeAmount) cup\(coffeeAmount == 1 ? "" : "s")")
+                }
+            }
+            .navigationBarTitle("BetterRest")
+            .navigationBarItems(trailing:
+                Button(action: calculateBedtime) {
+                    Text("Calculate")
+                }
+            )
+        }
+    }
+    
+    func calculateBedtime() {
+        
     }
 }
 
