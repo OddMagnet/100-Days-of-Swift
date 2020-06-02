539d6e7564b1b8d7d78d74bae794634def33dc59
OddMagnet <mibruenen@gmail.com>
2020-06-02 11:52:30 +0200

    Add list examples

--- 026-034 Expanding your skills/Project 5/WordScramble/WordScramble/ContentView.swift
+++ 026-034 Expanding your skills/Project 5/WordScramble/WordScramble/ContentView.swift
@@ -9,8 +9,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Section(header: Text("mixed list")) {
+                List {
+                    Section(header: Text("Section 1")) {
+                        Text("Static - 1")
+                        Text("Static - 2")
+                    }
+                    Section(header: Text("Section 2")) {
+                        ForEach(0 ..< 3) {
+                            Text("Dynamic - \($0)")
+                        }
+                    }
+                    Section(header: Text("Static - 3")) {
+                        Text("Static - 3")
+                        Text("Static - 4")
+                    }
+                    Section(header: Text("id example")) {
+                        ForEach(people, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                }
+                .listStyle(GroupedListStyle())
+            }
+            
+        }
     }
 }
 
