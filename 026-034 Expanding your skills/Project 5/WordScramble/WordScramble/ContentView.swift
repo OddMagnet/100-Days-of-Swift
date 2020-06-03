de6da0e96f4eebfefcd11cc6dc318b12bf731bc2
OddMagnet <mibruenen@gmail.com>
2020-06-03 16:31:19 +0200

    Add basic UI layout and functionality

--- 026-034 Expanding your skills/Project 5/WordScramble/WordScramble/ContentView.swift
+++ 026-034 Expanding your skills/Project 5/WordScramble/WordScramble/ContentView.swift
@@ -9,9 +9,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    // state
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
     
     var body: some View {
-        Text("Hello World!")
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addNewWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .autocapitalization(.none)
+
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle") // show length of each word
+                    Text($0)
+                }
+            }
+            .navigationBarTitle("WordScramble")//rootWord)
+        }
+    }
+    
+    func addNewWord() {
+        // lowercase and trim the word, to make sure no duplicate are added
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        // ensure the input was not empty
+        guard answer.count > 0 else {
+            return
+        }
+
+        // extra validation to come
+
+        // append at the beginning so new words are at the top of the list
+        usedWords.insert(answer, at: 0)
+        newWord = ""
     }
 }
 
