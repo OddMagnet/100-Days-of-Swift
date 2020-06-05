b135661c2b9638798386b2e26f4c1331c32d04fe
OddMagnet <mibruenen@gmail.com>
2020-06-05 11:15:02 +0200

    Add customizing animations example

--- 026-034 Expanding your skills/Project 6/Animations/Animations/ContentView.swift
+++ 026-034 Expanding your skills/Project 6/Animations/Animations/ContentView.swift
@@ -10,21 +10,45 @@ import SwiftUI
 
 struct ContentView: View {
     // state
-    @State private var animationAmount: CGFloat = 1
-    
+    @State private var animationAmount1: CGFloat = 1
+    @State private var animationAmount2: CGFloat = 1
+    @State private var animationAmount3: CGFloat = 1
+    @State private var animationAmount4: CGFloat = 1
+
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             // Implicit animation example
             Button("Tap Me") {
-                self.animationAmount += 1
+                self.animationAmount1 += 0.25
             }
-            .padding(50 + animationAmount*10)
+            .padding(50)
             .background(Color.red)
             .foregroundColor(.white)
             .clipShape(Circle())
-            //.scaleEffect(animationAmount)
-            .blur(radius: (animationAmount - 1) * 3)
+            .scaleEffect(animationAmount1)
+            .blur(radius: (animationAmount1 - 1) * 2)
             .animation(.default)
+            
+            // Customized implicit animation example
+            Button("See me") {
+            }
+            .padding(50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .overlay(
+                Circle()
+                    .stroke(Color.red)
+                    .scaleEffect(animationAmount2)
+                    .opacity(Double(2 - animationAmount2))
+                    .animation(
+                        Animation.easeOut(duration: 1)
+                            .repeatForever(autoreverses: false)
+                    )
+            )
+            .onAppear {
+                self.animationAmount2 = 1.2
+            }
         }
     }
 }
