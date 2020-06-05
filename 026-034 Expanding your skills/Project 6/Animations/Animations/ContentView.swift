e673d6e89c3e796d1294630be84cc7c8edfaa848
OddMagnet <mibruenen@gmail.com>
2020-06-05 11:35:20 +0200

    Add sections

--- 026-034 Expanding your skills/Project 6/Animations/Animations/ContentView.swift
+++ 026-034 Expanding your skills/Project 6/Animations/Animations/ContentView.swift
@@ -16,38 +16,40 @@ struct ContentView: View {
     @State private var animationAmount4: CGFloat = 1
 
     var body: some View {
-        VStack(spacing: 20) {
-            // Implicit animation example
-            Button("Tap Me") {
-                self.animationAmount1 += 0.25
+        Form {
+            Section(header: Text("Implicit animation example")) {
+                Button("Tap Me") {
+                    self.animationAmount1 += 0.25
+                }
+                .padding(50)
+                .background(Color.red)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .scaleEffect(animationAmount1)
+                .blur(radius: (animationAmount1 - 1) * 2)
+                .animation(.default)
             }
-            .padding(50)
-            .background(Color.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .scaleEffect(animationAmount1)
-            .blur(radius: (animationAmount1 - 1) * 2)
-            .animation(.default)
             
-            // Customized implicit animation example
-            Button("See me") {
-            }
-            .padding(50)
-            .background(Color.red)
-            .foregroundColor(.white)
-            .clipShape(Circle())
-            .overlay(
-                Circle()
-                    .stroke(Color.red)
-                    .scaleEffect(animationAmount2)
-                    .opacity(Double(2 - animationAmount2))
-                    .animation(
-                        Animation.easeOut(duration: 1)
-                            .repeatForever(autoreverses: false)
+            Section(header: Text("Customized implicit animation example")) {
+                Button("See me") {
+                }
+                .padding(50)
+                .background(Color.red)
+                .foregroundColor(.white)
+                .clipShape(Circle())
+                .overlay(
+                    Circle()
+                        .stroke(Color.red)
+                        .scaleEffect(animationAmount2)
+                        .opacity(Double(2 - animationAmount2))
+                        .animation(
+                            Animation.easeOut(duration: 1)
+                                .repeatForever(autoreverses: false)
                     )
-            )
-            .onAppear {
-                self.animationAmount2 = 1.2
+                )
+                    .onAppear {
+                        self.animationAmount2 = 1.2
+                }
             }
         }
     }
