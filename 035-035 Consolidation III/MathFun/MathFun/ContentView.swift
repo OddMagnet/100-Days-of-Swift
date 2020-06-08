cf9209173f75eca2dad2c165d79b6910fbfdea36
OddMagnet <mibruenen@gmail.com>
2020-06-08 17:05:12 +0200

    Initial commit

---
+++ 035-035 Consolidation III/MathFun/MathFun/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MathFun
+//
+//  Created by Michael Brünen on 08.06.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
