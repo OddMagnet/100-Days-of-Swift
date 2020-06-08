05ea38bd11b7fb61d77bf7abde379ccfd111354f
OddMagnet <mibruenen@gmail.com>
2020-06-08 22:39:11 +0200

    Complete basic game functionality

--- 035-035 Consolidation III/MathFun/MathFun/GameView.swift
+++ 035-035 Consolidation III/MathFun/MathFun/GameView.swift
@@ -9,30 +9,68 @@
 import SwiftUI
 
 struct GameView: View {
-    // state
+    // bindings
     @Binding var gameRunning: Bool
     @Binding var questions: [Question]
+    
+    // state
     @State private var currentQuestion = 0
+    @State private var score = 0
+    @State private var userAnswer = ""
+    @State private var gameOver = false     // show alert after last question
     
     var body: some View {
-        VStack {
-            Text(questions[currentQuestion].question)
+        VStack(spacing: 20) {
             Spacer()
-            Button("Settings") {
-                self.gameRunning.toggle()
-            }
+            Text("Question \(currentQuestion + 1)/\(questions.count)")
+                .font(.headline)
+            Text(questions[currentQuestion].question)
+                .font(.largeTitle)
+            TextField("Your answer", text: $userAnswer, onCommit: checkAnswer)
+                .frame(width: 300, height: 40, alignment: .center)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .keyboardType(.numberPad)
+            Spacer()
+            Text("Score \(score)")
+                .font(.title)
         }
         .navigationBarTitle("MathFun")
         .navigationBarItems(trailing: Button("Settings") {
             self.gameRunning.toggle()
         })
+        .alert(isPresented: $gameOver) {
+            Alert(title: Text("Game Over"),
+                  message: Text("You answered all the questions"),
+                  dismissButton: .cancel(Text("Menu")){
+                    self.gameRunning = false
+                })
+        }
+    }
+    
+    func checkAnswer() {
+        // calculate new score
+        if Int(userAnswer) == questions[currentQuestion].answer {
+            score += 2
+        } else {
+            score -= 1
+        }
+        
+        // show next question or gameOver
+        if (currentQuestion + 1) == questions.count {
+            gameOver = true
+        } else {
+            currentQuestion += 1
+        }
+        
+        // reset textfield
+        userAnswer = ""
     }
 }
 
 
 struct GameView_Previews: PreviewProvider {
     @State static var gameRunning = true
-    @State static var questions = [Question(question: "Test", answer: 42)]
+    @State static var questions = [Question(question: "What is 6 x 7", answer: 42)]
     static var previews: some View {
         GameView(
             gameRunning: $gameRunning,
