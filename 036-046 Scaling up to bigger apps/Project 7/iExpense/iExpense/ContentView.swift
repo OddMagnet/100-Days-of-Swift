6dc4081b4e4323918320cc0045e1bdb3da505336
OddMagnet <mibruenen@gmail.com>
2020-06-09 23:33:16 +0200

    Initial commit

---
+++ 036-046 Scaling up to bigger apps/Project 7/iExpense/iExpense/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpense
+//
+//  Created by Michael Brünen on 09.06.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
