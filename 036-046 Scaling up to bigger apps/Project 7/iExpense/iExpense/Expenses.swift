93b278ece8b7bff02a4e5e93a368a0ba10be73db
OddMagnet <mibruenen@gmail.com>
2020-06-10 13:33:45 +0200

    Add loading and saving

--- 036-046 Scaling up to bigger apps/Project 7/iExpense/iExpense/Expenses.swift
+++ 036-046 Scaling up to bigger apps/Project 7/iExpense/iExpense/Expenses.swift
@@ -9,5 +9,25 @@
 import Foundation
 
 class Expenses: ObservableObject {
-    @Published var items = [ExpenseItem]()
+    @Published var items: [ExpenseItem] {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        
+        // create empty instance if no existing data is found
+        self.items = []
+    }
 }
