dd30ec8b4efc71e865e09cb909a7dbd81b00c127
OddMagnet <mibruenen@gmail.com>
2020-06-10 09:10:24 +0200

    Add base structure for expense items

---
+++ 036-046 Scaling up to bigger apps/Project 7/iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,15 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Michael Brünen on 10.06.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import Foundation
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let cost: Int
+}
