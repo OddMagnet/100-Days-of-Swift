aba15c0221064eea0a1fdd42a280ed2ce87a430c
OddMagnet <mibruenen@gmail.com>
2020-06-12 20:07:47 +0200

    Add examples

--- 036-046 Scaling up to bigger apps/Project 8/Moonshot/Moonshot/Examples.swift
+++ 036-046 Scaling up to bigger apps/Project 8/Moonshot/Moonshot/Examples.swift
@@ -9,8 +9,70 @@
 import SwiftUI
 
 struct Examples: View {
+    struct User: Codable {
+        var name: String
+        var address: Address
+    }
+    struct Address: Codable {
+        var street: String
+        var city: String
+    }
+    @State private var user = User(name: "None", address: Address(street: "None", city: "None"))
+    let input = """
+    {
+        "name": "Taylor Swift",
+        "address": {
+            "street": "555, Taylor Swift Avenue",
+            "city": "Nashville"
+        }
+    }
+    """
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(spacing: 10) {
+                Section(header: Text("GeometryReader example").font(.headline)){
+                    GeometryReader { geo in
+                        Image("Example")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: geo.size.width)
+                    }
+                }
+                
+                Section(header: Text("ScrollView example").font(.headline)) {
+                    ScrollView(.vertical) {
+                        VStack(spacing: 10){
+                            ForEach(0..<100) {
+                                Text("Item \($0)")
+                            }
+                        }
+                        .frame(maxWidth: .infinity)
+                    }
+                }
+                
+                Section(header: Text("NavigationLink example").font(.headline)) {
+                    NavigationLink(destination: Text("Detail View")) {
+                        Text("Master View")
+                    }
+                }
+                
+                Section(header: Text("Hierachical Codable data example").font(.headline)) {
+                    Text(input)
+                        .disabled(true)
+                    Button("Decode JSON") {
+                        let data = Data(self.input.utf8)
+                        let decoder = JSONDecoder()
+                        if let decoded = try? decoder.decode(User.self, from: data) {
+                            self.user = decoded
+                        }
+                    }
+                    
+                    Text("Name: \(user.name) \nAddress: \(user.address.street) in \(user.address.city)")
+                }
+            }
+            .navigationBarTitle("Examples")
+        }
     }
 }
 
