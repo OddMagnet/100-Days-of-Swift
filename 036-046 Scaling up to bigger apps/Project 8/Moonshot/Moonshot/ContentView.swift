056a8d06284178780a3e58e8d1571219f6e45197
OddMagnet <mibruenen@gmail.com>
2020-06-12 13:31:01 +0200

    Initial commit

---
+++ 036-046 Scaling up to bigger apps/Project 8/Moonshot/Moonshot/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Michael Brünen on 12.06.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 036-046 Scaling up to bigger apps/Project 8/Moonshot/Moonshot/Examples.swift
@@ -0,0 +1,21 @@
+//
+//  Examples.swift
+//  Moonshot
+//
+//  Created by Michael Brünen on 12.06.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+struct Examples: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Examples_Previews: PreviewProvider {
+    static var previews: some View {
+        Examples()
+    }
+}
