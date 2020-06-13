758112355c49d462b4a17badb8f8759d67b20e42
OddMagnet <mibruenen@gmail.com>
2020-06-13 09:35:18 +0200

    Add Mission type

---
+++ 036-046 Scaling up to bigger apps/Project 8/Moonshot/Moonshot/Mission.swift
@@ -0,0 +1,21 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Michael Brünen on 13.06.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+    
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
