96f9c074db70bcd39ad862292510a254e8ee44c5
OddMagnet <mibruenen@gmail.com>
2020-06-13 09:44:47 +0200

    Add computed properties for missions

--- 036-046 Scaling up to bigger apps/Project 8/Moonshot/Moonshot/Mission.swift
+++ 036-046 Scaling up to bigger apps/Project 8/Moonshot/Moonshot/Mission.swift
@@ -18,4 +18,12 @@ struct Mission: Codable, Identifiable {
     let launchDate: String?
     let crew: [CrewRole]
     let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
 }
