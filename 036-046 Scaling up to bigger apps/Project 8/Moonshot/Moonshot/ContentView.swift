15b59333b87a8520770650eb532b9111983c605a
OddMagnet <mibruenen@gmail.com>
2020-06-13 09:29:43 +0200

    Load astronauts

--- 036-046 Scaling up to bigger apps/Project 8/Moonshot/Moonshot/ContentView.swift
+++ 036-046 Scaling up to bigger apps/Project 8/Moonshot/Moonshot/ContentView.swift
@@ -9,8 +9,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    let astronauts = Bundle.main.decode("astronauts.json")
+    
     var body: some View {
-        Text("Hello, World!")
+        Text("Astronauts: \(astronauts.count)")
     }
 }
 
