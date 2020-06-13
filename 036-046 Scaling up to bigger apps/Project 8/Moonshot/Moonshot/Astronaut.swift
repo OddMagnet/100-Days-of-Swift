f70b9f96e2732886353d39625581b70785929b1b
OddMagnet <mibruenen@gmail.com>
2020-06-13 09:22:18 +0200

    Add Astronaut type

---
+++ 036-046 Scaling up to bigger apps/Project 8/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,15 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Michael Brünen on 13.06.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
