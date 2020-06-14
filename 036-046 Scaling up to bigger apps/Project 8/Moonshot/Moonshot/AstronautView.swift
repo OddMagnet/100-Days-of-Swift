ad14a57eb7b94dfc849f362dd48f4c1c8ea54e29
OddMagnet <mibruenen@gmail.com>
2020-06-14 06:39:56 +0200

    Fix some descriptions not showing in full

--- 036-046 Scaling up to bigger apps/Project 8/Moonshot/Moonshot/AstronautView.swift
+++ 036-046 Scaling up to bigger apps/Project 8/Moonshot/Moonshot/AstronautView.swift
@@ -22,6 +22,7 @@ struct AstronautView: View {
                     
                     Text(self.astronaut.description)
                         .padding()
+                        .layoutPriority(1)
                 }
             }
         }
