0233f4ee9a7ba973965975e6c94c67a7df0feaa4
OddMagnet <mibruenen@gmail.com>
2020-06-14 06:25:05 +0200

    Add AstronautView

---
+++ 036-046 Scaling up to bigger apps/Project 8/Moonshot/Moonshot/AstronautView.swift
@@ -0,0 +1,37 @@
+//
+//  AstronautView.swift
+//  Moonshot
+//
+//  Created by Michael Brünen on 14.06.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+    
+    var body: some View {
+        GeometryReader { geo in
+            ScrollView(.vertical) {
+                VStack {
+                    Image(self.astronaut.id)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geo.size.width)
+                    
+                    Text(self.astronaut.description)
+                        .padding()
+                }
+            }
+        }
+        .navigationBarTitle(Text(self.astronaut.name), displayMode: .inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    static var previews: some View {
+        AstronautView(astronaut: astronauts[0])
+    }
+}
