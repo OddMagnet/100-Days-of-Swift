33f02772bb7188e946f7d309a4fd89625bef3829
OddMagnet <mibruenen@gmail.com>
2020-06-19 19:06:38 +0200

    Update variable names

--- 047-048 Consolidation IV/iTrack/iTrack/AddActivityView.swift
+++ 047-048 Consolidation IV/iTrack/iTrack/AddActivityView.swift
@@ -18,12 +18,12 @@ struct AddActivityView: View {
         NavigationView {
             Form {
                 TextField("Name", text: $activityName)
-                TextField("Name", text: $activityDescription)
+                TextField("Description", text: $activityDescription)
             }
             .navigationBarTitle("Add new activity")
             .navigationBarItems(trailing: Button("Save") {
                 let activity = Activity(name: self.activityName, description: self.activityDescription)
-                self.activities.activities.append(activity)
+                self.activities.items.append(activity)
                 self.presentationMode.wrappedValue.dismiss()
             })
         }
@@ -31,7 +31,7 @@ struct AddActivityView: View {
 }
 
 struct AddActivityView_Previews: PreviewProvider {
-    static var activities = Activities(activities: loadData())
+    static var activities = Activities()
     static var previews: some View {
         AddActivityView(activities: activities)
     }
