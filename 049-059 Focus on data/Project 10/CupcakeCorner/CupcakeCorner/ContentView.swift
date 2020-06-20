9d5991916c4ab343db64365fdebae7ccf0360124
OddMagnet <mibruenen@gmail.com>
2020-06-20 14:04:04 +0200

    Initial commit

---
+++ 049-059 Focus on data/Project 10/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Michael Brünen on 20.06.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
