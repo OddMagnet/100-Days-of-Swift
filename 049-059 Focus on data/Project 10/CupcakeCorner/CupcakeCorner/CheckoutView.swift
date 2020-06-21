0483dc1cd33323d7caf464282e73b1e731d67b1d
OddMagnet <mibruenen@gmail.com>
2020-06-21 17:55:23 +0200

    Add CheckoutView

---
+++ 049-059 Focus on data/Project 10/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,41 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Michael Brünen on 21.06.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+
+    var body: some View {
+        GeometryReader { geometry in
+            ScrollView {
+                VStack {
+                    Image("cupcakes")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: geometry.size.width)
+                    
+                    Text("Your total is $\(self.order.cost, specifier: "%.2f")")
+                        .font(.title)
+
+                    Button("Place Order") {
+                        // place the order
+                    }
+                    .padding()
+                }
+            }
+        }
+        .navigationBarTitle("Check out", displayMode: .inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
