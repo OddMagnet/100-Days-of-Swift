ce0367316a8e37c4cd8b563fc8d746245b965147
OddMagnet <mibruenen@gmail.com>
2020-06-22 14:47:09 +0200

    Finish basic UI

--- 049-059 Focus on data/Project 10/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ 049-059 Focus on data/Project 10/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -12,7 +12,47 @@ struct ContentView: View {
     @ObservedObject var order = Order()
     
     var body: some View {
-        ExamplesView()
+        //        ExamplesView()
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(0..<Order.types.count) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper(value: $order.quantity, in: 3...20) {
+                        Text("Number of cakes: \(order.quantity)")
+                    }
+                }
+                
+                Section {
+                    Section {
+                        Toggle(isOn: $order.specialRequestEnabled.animation()) {
+                            Text("Any special requests?")
+                        }
+                        
+                        if order.specialRequestEnabled {
+                            Toggle(isOn: $order.extraFrosting) {
+                                Text("Add extra frosting")
+                            }
+                            
+                            Toggle(isOn: $order.addSprinkles) {
+                                Text("Add extra sprinkles")
+                            }
+                        }
+                    }
+                }
+                
+                Section {
+                    NavigationLink("Delivery details", destination:
+                        AddressView(order: order)
+                    )
+                }
+            }
+            .navigationBarTitle("Cupcake Corner")
+        }
     }
 }
 
