71bb4bdf26de256c6ef9171332ca80babca33cab
OddMagnet <mibruenen@gmail.com>
2020-06-22 14:46:45 +0200

    Place order and handle reply

--- 049-059 Focus on data/Project 10/CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ 049-059 Focus on data/Project 10/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 
 struct CheckoutView: View {
     @ObservedObject var order: Order
-
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
     var body: some View {
         GeometryReader { geometry in
             ScrollView {
@@ -22,15 +24,54 @@ struct CheckoutView: View {
                     
                     Text("Your total is $\(self.order.cost, specifier: "%.2f")")
                         .font(.title)
-
+                    
                     Button("Place Order") {
-                        // place the order
+                        self.placeOrder()
                     }
                     .padding()
                 }
             }
         }
         .navigationBarTitle("Check out", displayMode: .inline)
+        .alert(isPresented: $showingConfirmation) {
+            Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+        }
+    }
+    
+    func placeOrder() {
+        // Convert current order object into JSON data
+        let encoder = JSONEncoder()
+        guard let encodedData = try? encoder.encode(order) else {
+            print("Failed to encode order data")
+            return
+        }
+        
+        // Prepare a URLRequest to send encoded data as JSON
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        // Configure the request
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encodedData
+        
+        // Run the request and process the response
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            // handle the response
+            // ensure data was returned
+            guard let data = data else {
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error").")
+                return
+            }
+            // decode the returned data and display it in an alert
+            let decoder = JSONDecoder()
+            if let decodedOrder = try? decoder.decode(Order.self, from: data) {
+                self.confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+                self.showingConfirmation = true
+            } else {
+                print("Invalid response from server")
+            }
+            
+        }.resume()
     }
 }
 
