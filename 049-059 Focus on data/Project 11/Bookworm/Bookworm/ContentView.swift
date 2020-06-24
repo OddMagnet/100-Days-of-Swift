02299d5d4c5973ddf61320fe4c9d2db7d22ccaec
OddMagnet <mibruenen@gmail.com>
2020-06-24 17:07:02 +0200

    Add CoreData example

--- 049-059 Focus on data/Project 11/Bookworm/Bookworm/ContentView.swift
+++ 049-059 Focus on data/Project 11/Bookworm/Bookworm/ContentView.swift
@@ -11,11 +11,34 @@ import SwiftUI
 struct ContentView: View {
     @State private var rememberMe = false
     
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Student.entity(), sortDescriptors: []) var students: FetchedResults<Student>
+    
     var body: some View {
         VStack {
             PushButton(title: "Remember me", isOn: $rememberMe)
             Text(rememberMe ? "On" : "Off")
             SizeClassTracker()
+            VStack {
+                List {
+                    ForEach(students, id: \.id) { student in
+                        Text(student.name ?? "Unknown")
+                    }
+                }
+                Button("Add") {
+                    let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+                    let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+
+                    let chosenFirstName = firstNames.randomElement()!
+                    let chosenLastName = lastNames.randomElement()!
+
+                    let student = Student(context: self.moc)
+                    student.id = UUID()
+                    student.name = "\(chosenFirstName) \(chosenLastName)"
+                    
+                    try? self.moc.save()
+                }
+            }
         }
     }
 }
