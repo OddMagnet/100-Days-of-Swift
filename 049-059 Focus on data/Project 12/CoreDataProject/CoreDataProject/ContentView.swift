4b41031bb6cff750434b6889e1625052ae8743b8
OddMagnet <mibruenen@gmail.com>
2020-06-26 17:59:01 +0200

    Initial commit

---
+++ 049-059 Focus on data/Project 12/CoreDataProject/CoreDataProject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreDataProject
+//
+//  Created by Michael Brünen on 26.06.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
