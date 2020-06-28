c0983143a211accba6c7a6a3ee91e284954b7324
OddMagnet <mibruenen@gmail.com>
2020-06-29 01:27:22 +0200

    Add UserInfoKey extension

---
+++ 060-061 Consolidation V/FriendFace/FriendFace/Extensions/CodingUserInfoKey+context.swift
@@ -0,0 +1,13 @@
+//
+//  CodingUserInfoKey+context.swift
+//  FriendFace
+//
+//  Created by Michael Brünen on 29.06.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import Foundation
+
+extension CodingUserInfoKey {
+   static let context = CodingUserInfoKey(rawValue: "context")
+}
