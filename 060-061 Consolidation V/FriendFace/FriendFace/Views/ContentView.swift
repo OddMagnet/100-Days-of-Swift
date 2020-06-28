231fa5a4654c89db2ecee93ca48bd48bdc4444db
OddMagnet <mibruenen@gmail.com>
2020-06-28 16:04:52 +0200

    Load data on view appearance

--- 060-061 Consolidation V/FriendFace/FriendFace/Views/ContentView.swift
+++ 060-061 Consolidation V/FriendFace/FriendFace/Views/ContentView.swift
@@ -17,7 +17,36 @@ struct ContentView: View {
                 Text(user.name)
             }
             .navigationBarTitle("FriendFace")
+            .onAppear(perform: loadData)
         }
+    }
+    
+    func loadData() {
+        // prepare the request for data
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+            fatalError("Failed to create data url")
+        }
+        let request = URLRequest(url: url)
+        
+        // request the data
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            // check if data is present
+            if let data = data {
+                // if so, handle it
+                let decoder = JSONDecoder()
+                decoder.dateDecodingStrategy = .iso8601
+                if let decodedResponse = try? decoder.decode([User].self, from: data) {
+                    // on success, update the main queue with new data
+                    DispatchQueue.main.async {
+                        self.users = decodedResponse
+                    }
+                    // everything done
+                    return
+                }
+            }
+            // if we're still here it means there was a problem
+            print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+        }.resume()
     }
 }
 
