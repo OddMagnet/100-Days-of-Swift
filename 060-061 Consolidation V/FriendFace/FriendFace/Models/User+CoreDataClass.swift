ef991ab71831f119c8d8148fa8f43ba487fc3a30
OddMagnet <mibruenen@gmail.com>
2020-06-29 02:12:59 +0200

    Fix EntityName

--- 060-061 Consolidation V/FriendFace/FriendFace/Models/User+CoreDataClass.swift
+++ 060-061 Consolidation V/FriendFace/FriendFace/Models/User+CoreDataClass.swift
@@ -40,7 +40,7 @@ public class User: NSManagedObject, Identifiable, Codable {
     required convenience public init(from decoder: Decoder) throws {
         // get the context and the entity in the context
         guard let context = decoder.userInfo[CodingUserInfoKey.context!] as? NSManagedObjectContext else { fatalError("Could not get context") }
-        guard let entity = NSEntityDescription.entity(forEntityName: "FriendFace", in: context) else { fatalError("Could not get entity") }
+        guard let entity = NSEntityDescription.entity(forEntityName: "User", in: context) else { fatalError("Could not get entity") }
         
         // init self
         self.init(entity: entity, insertInto: context)
