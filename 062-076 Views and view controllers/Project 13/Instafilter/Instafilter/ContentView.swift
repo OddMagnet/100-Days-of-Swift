d90713c8b441dfce5ac161b3dd5a64104b63e068
OddMagnet <mibruenen@gmail.com>
2020-06-30 16:54:30 +0200

    Initial commit

---
+++ 062-076 Views and view controllers/Project 13/Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Michael Brünen on 30.06.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Examples()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
