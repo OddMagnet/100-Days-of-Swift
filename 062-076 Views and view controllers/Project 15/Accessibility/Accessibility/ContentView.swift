c1a058c0e26537d5b3a5a69edee2a00d5cac4a84
OddMagnet <mibruenen@gmail.com>
2020-07-09 14:16:47 +0200

    Add example for identifying views with labels

--- 062-076 Views and view controllers/Project 15/Accessibility/Accessibility/ContentView.swift
+++ 062-076 Views and view controllers/Project 15/Accessibility/Accessibility/ContentView.swift
@@ -9,8 +9,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    let pictures = [
+        "ales-krivec-15949",
+        "galina-n-189483",
+        "kevin-horstmann-141705",
+        "nicolas-tissot-335096"
+    ]
+    let labels = [
+        "Tulips",
+        "Frozen tree buds",
+        "Sunflowers",
+        "Fireworks",
+    ]
+    @State private var selectedPicture = Int.random(in: 0...3)
     var body: some View {
-        Text("Hello, World!")
+        Image(pictures[selectedPicture])
+            .resizable()
+            .scaledToFit()
+            .accessibility(label: Text(labels[selectedPicture]))
+            .accessibility(addTraits: .isButton)
+            .onTapGesture {
+                self.selectedPicture = Int.random(in: 0...3)
+        }
     }
 }
 
