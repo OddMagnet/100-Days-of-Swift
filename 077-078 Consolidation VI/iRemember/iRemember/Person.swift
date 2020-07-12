c9aacc72fed46e376329a853b1c0ffd61fceb47d
OddMagnet <mibruenen@gmail.com>
2020-07-12 21:22:58 +0200

    Add conformance to Identifiable and value initializer

--- 077-078 Consolidation VI/iRemember/iRemember/Person.swift
+++ 077-078 Consolidation VI/iRemember/iRemember/Person.swift
@@ -8,13 +8,14 @@
 
 import SwiftUI
 
-struct Person: Codable, Comparable {
+struct Person: Codable, Comparable, Identifiable {
+    let id: UUID
     let firstName: String
     let lastName: String
     let image: UIImage
     
     enum CodingKeys: CodingKey {
-        case firstName, lastName, image
+        case id, firstName, lastName, image
     }
     
     static func < (lhs: Self, rhs: Self) -> Bool {
@@ -24,15 +25,24 @@ struct Person: Codable, Comparable {
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         
+        id = try container.decode(UUID.self, forKey: .id)
         firstName = try container.decode(String.self, forKey: .firstName)
         lastName = try container.decode(String.self, forKey: .lastName)
         let imageData = try container.decode(Data.self, forKey: .image)
         image = UIImage(data: imageData)!
     }
     
+    init(firstName: String, lastName: String, image: UIImage) {
+        self.id = UUID()
+        self.firstName = firstName
+        self.lastName = lastName
+        self.image = image
+    }
+    
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         
+        try container.encode(id, forKey: .id)
         try container.encode(firstName, forKey: .firstName)
         try container.encode(lastName, forKey: .firstName)
         let imageData = image.jpegData(compressionQuality: 0.8)
