05304f5e0bb10c2d11fb4c47aae8b5724b7f72b0
OddMagnet <mibruenen@gmail.com>
2020-07-13 10:49:51 +0200

    Add location and image picker source selection

--- 077-078 Consolidation VI/iRemember/iRemember/AddPersonView.swift
+++ 077-078 Consolidation VI/iRemember/iRemember/AddPersonView.swift
@@ -7,15 +7,18 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct AddPersonView: View {
     @Environment(\.presentationMode) var presentationMode
     @Binding var firstName: String
     @Binding var lastName: String
     @Binding var image: UIImage?
+    @Binding var location: CLLocationCoordinate2D
     @State private var showingImagePicker = false
     @State private var sourceType: UIImagePickerController.SourceType = .photoLibrary
     
+    let locationFetcher = LocationFetcher()
     var canSave: Bool {
         image != nil && !firstName.isEmpty && !lastName.isEmpty
     }
@@ -29,20 +32,28 @@ struct AddPersonView: View {
                             .resizable()
                             .scaledToFit()
                     } else {
+                        // using the normal action closure on the buttons result in both activating
+                        // this seems to be a bug in SwiftUI, so onTapGesture is used as a work-around
                         HStack {
-                            Button("Select photo") {
+                            Button(action: {}) {
+                                Text("Select photo")
+                            }
+                            .padding()
+                            .onTapGesture {
                                 self.sourceType = .photoLibrary
                                 self.showingImagePicker = true
                             }
-                            .padding()
                             
                             Spacer()
                             
-                            Button("Take photo") {
+                            Button(action: {}) {
+                                Text("Take photo")
+                            }
+                            .padding()
+                            .onTapGesture {
                                 self.sourceType = .camera
                                 self.showingImagePicker = true
                             }
-                            .padding()
                         }
                     }
                 }
@@ -55,18 +66,27 @@ struct AddPersonView: View {
             Spacer()
             
             Button("Save") {
+                if let location = self.locationFetcher.lastKnownLocation {
+                    self.location = location
+                }
                 self.presentationMode.wrappedValue.dismiss()
             }
+            .padding()
             .disabled(!canSave)
         }
+        .onAppear(perform: startTracking)
         .sheet(isPresented: $showingImagePicker) {
-            ImagePicker(image: self.$image, sourceType: self.sourceType)
+            ImagePicker(image: self.$image, sourceType: self.$sourceType)
         }
+    }
+    
+    func startTracking() {
+        self.locationFetcher.start()
     }
 }
 
 struct AddPersonView_Previews: PreviewProvider {
     static var previews: some View {
-        AddPersonView(firstName: .constant("Test"), lastName: .constant("Name"), image: .constant(nil))
+        AddPersonView(firstName: .constant("Test"), lastName: .constant("Name"), image: .constant(nil), location: .constant(CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)))
     }
 }
