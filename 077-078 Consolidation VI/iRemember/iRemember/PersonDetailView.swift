63d6e698ad624c980316ffd07b654cc5e232c27d
OddMagnet <mibruenen@gmail.com>
2020-07-13 12:28:11 +0200

    Add segmented control and coordinate view

--- 077-078 Consolidation VI/iRemember/iRemember/PersonDetailView.swift
+++ 077-078 Consolidation VI/iRemember/iRemember/PersonDetailView.swift
@@ -7,27 +7,50 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct PersonDetailView: View {
     var person: Person
     
+    @State private var selectedSubView = "Image"
+    private let subViews = ["Image", "Location"]
+    
     var body: some View {
         VStack {
-            Image(uiImage: person.image)
-                .resizable()
-                .scaledToFit()
-                .padding()
-                .navigationBarTitle("\(person.firstName) \(person.lastName)")
+            Picker(selection: $selectedSubView, label: Text("Select")) {
+                ForEach(subViews, id: \.self) {
+                    Text($0)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            .padding()
             
+            if selectedSubView == "Image" {
+                Image(uiImage: person.image)
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                    .animation(.linear)
+                    .transition(.asymmetric(insertion: .slide, removal: .move(edge: .leading)))
+            } else {
+                VStack {
+                    Text("Longitude: \(person.location.longitude)")
+                    Text("Latitude: \(person.location.latitude)")
+                }
+                .padding()
+                .animation(.linear)
+                .transition(.move(edge: .trailing))
+            }
             Spacer()
         }
+        .navigationBarTitle("\(person.firstName) \(person.lastName)")
     }
 }
 
 struct PersonDetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            PersonDetailView(person: Person(firstName: "Test", lastName: "Person", image: UIImage(systemName: "camera")!))
+            PersonDetailView(person: Person(firstName: "Test", lastName: "Person", image: UIImage(systemName: "camera")!, location: Coordinate(latitude: 0.0, longitude: 0.0)))
         }
     }
 }
