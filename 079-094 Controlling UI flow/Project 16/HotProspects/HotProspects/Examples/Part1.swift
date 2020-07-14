bfb11db166452a46fe299da169f6e352d191d3df
OddMagnet <mibruenen@gmail.com>
2020-07-14 15:09:18 +0200

    Add example for Reading custom values from the environment with @EnvironmentObject

--- 079-094 Controlling UI flow/Project 16/HotProspects/HotProspects/Examples/Part1.swift
+++ 079-094 Controlling UI flow/Project 16/HotProspects/HotProspects/Examples/Part1.swift
@@ -8,19 +8,53 @@
 
 import SwiftUI
 
+class User: ObservableObject {
+    @Published var name = "Odd Magnet"
+}
+
+struct EditView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        TextField("Name", text: $user.name)
+    }
+}
+
+struct DisplayView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        Text(user.name)
+    }
+}
+
 struct Part1: View {
+    let user = User()
+    
     var body: some View {
         Section(header: Text("Part 1")) {
             
-            NavigationLink("Placeholder", destination:
-                Text("Placeholder")
-            )            
+            NavigationLink("Reading custom values from the environment with @EnvironmentObject", destination:
+                VStack(alignment: .leading) {
+                    EditView()
+                        .padding()
+                        .border(Color.blue)
+                    DisplayView()
+                        .padding()
+                }
+                .padding()
+                .environmentObject(user)
+            )
         }
     }
 }
 
 struct Part1_Previews: PreviewProvider {
     static var previews: some View {
-        Part1()
+        NavigationView {
+            List {
+                Part1()
+            }
+        }
     }
 }
