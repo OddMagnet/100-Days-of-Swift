0e0e120693460c9d9b7e50698fa1859f5a63f711
OddMagnet <mibruenen@gmail.com>
2020-07-17 13:24:17 +0200

    Add QR code generation

--- 079-094 Controlling UI flow/Project 16/HotProspects/HotProspects/MeView.swift
+++ 079-094 Controlling UI flow/Project 16/HotProspects/HotProspects/MeView.swift
@@ -7,10 +7,55 @@
 //
 
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct MeView: View {
+    @State private var name = "Anonymous"
+    @State private var emailAddress = "you@yoursite.com"
+    
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                TextField("Name", text: $name)
+                    .textContentType(.name)
+                    .font(.title)
+                    .padding(.horizontal)
+                
+                TextField("Email Address", text: $emailAddress)
+                    .textContentType(.emailAddress)
+                    .font(.title)
+                    .padding([.horizontal, .bottom])
+                    .autocapitalization(.none)
+                
+                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+
+                Spacer()
+            }
+            .navigationBarTitle("Your code")
+        }
+    }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        // convert data and pass it to the filter
+        let data = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+        
+        // get output, convert to UIImage and return it
+        if let outputImage = filter.outputImage {
+            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgImage)
+            }
+        }
+        
+        // if the above failed, return the xmark.circle symbol
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
     }
 }
 
