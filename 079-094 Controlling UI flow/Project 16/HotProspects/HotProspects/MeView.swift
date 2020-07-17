829f491332cda80be1930646e2539626111b7757
OddMagnet <mibruenen@gmail.com>
2020-07-17 11:49:10 +0200

    Add basic UI

---
+++ 079-094 Controlling UI flow/Project 16/HotProspects/HotProspects/MeView.swift
@@ -0,0 +1,21 @@
+//
+//  MeView.swift
+//  HotProspects
+//
+//  Created by Michael Brünen on 17.07.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+struct MeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MeView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeView()
+    }
+}
---
+++ 079-094 Controlling UI flow/Project 16/HotProspects/HotProspects/ProspectsView.swift
@@ -0,0 +1,41 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by Michael Brünen on 17.07.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+enum FilterType {
+    case none, contacted, uncontacted
+}
+
+struct ProspectsView: View {
+    let filter: FilterType
+    
+    var title: String {
+        switch filter {
+        case .none:
+            return "Everyone"
+        case .contacted:
+            return "Contacted people"
+        case .uncontacted:
+            return "Uncontacted people"
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationBarTitle(title)
+        }
+    }
+}
+
+struct ProspectsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProspectsView(filter: .none)
+    }
+}
