623c1e1c57ed57ea45d4cb92411b8cc38691c529
OddMagnet <mibruenen@gmail.com>
2020-07-20 15:46:18 +0200

    Add card dragging animation, add removal closure

--- 079-094 Controlling UI flow/Project 17/Flashzilla/Flashzilla/FlashcardView.swift
+++ 079-094 Controlling UI flow/Project 17/Flashzilla/Flashzilla/FlashcardView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 
 struct FlashcardView: View {
     @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
     let card: Flashcard
+    var removal: (() -> Void)? = nil
     
     var body: some View {
         ZStack {
@@ -33,9 +35,30 @@ struct FlashcardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(Double(offset.width / 5))) // rotate slowly, 1/5 of the actual movement
+        .offset(x: offset.width * 5, y: 0)                  // move fast horiziontally, 5x the movement
+        .opacity(opacity(for: offset, after: 100))          // opacity goes down after 50 units
         .onTapGesture {
             self.isShowingAnswer.toggle()
         }
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    self.offset = gesture.translation
+                }
+                .onEnded { _ in
+                    if abs(self.offset.width) > 100 {
+                        self.removal?()
+                    } else {
+                        self.offset = .zero
+                    }
+                }
+        )
+    }
+    
+    func opacity(for offset: CGSize, after units: Int) -> Double {
+        let noFadeValue: Double = 1 + Double(units / 100)
+        return noFadeValue - Double(abs(offset.width / CGFloat(units / 2)))
     }
 }
 
