dfdea0c5f227d388b5bd2c533a3cb3793f6dd9ad
OddMagnet <mibruenen@gmail.com>
2020-07-21 15:43:16 +0200

    Add EditCardsView

---
+++ 079-094 Controlling UI flow/Project 17/Flashzilla/Flashzilla/EditCardsView.swift
@@ -0,0 +1,85 @@
+//
+//  EditCardsView.swift
+//  Flashzilla
+//
+//  Created by Michael Brünen on 21.07.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+struct EditCardsView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State private var cards = [Flashcard]()
+    @State private var newPrompt = ""
+    @State private var newAnswer = ""
+    
+    var body: some View {
+        NavigationView {
+            List {
+                Section(header: Text("Add new card")) {
+                    TextField("Prompt", text: $newPrompt)
+                    TextField("Answer", text: $newAnswer)
+                    Button("Add card", action: addCard)
+                }
+                
+                Section {
+                    ForEach(0 ..< cards.count, id: \.self) { index in
+                        VStack(alignment: .leading) {
+                            Text(self.cards[index].prompt)
+                                .font(.headline)
+                            Text(self.cards[index].answer)
+                                .foregroundColor(.secondary)
+                        }
+                    }
+                    .onDelete(perform: removeCards)
+                }
+            }
+            .navigationViewStyle(StackNavigationViewStyle())
+            .navigationBarTitle("Edit Cards")
+            .navigationBarItems(trailing: Button("Done", action: dismiss))
+            .listStyle(GroupedListStyle())
+            .onAppear(perform: loadData)
+        }
+    }
+    
+    func dismiss() {
+        self.presentationMode.wrappedValue.dismiss()
+    }
+    
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "Cards") {
+            if let decoded = try? JSONDecoder().decode([Flashcard].self, from: data) {
+                self.cards = decoded
+            }
+        }
+    }
+    
+    func saveData() {
+        if let data = try? JSONEncoder().encode(cards) {
+            UserDefaults.standard.set(data, forKey: "Cards")
+        }
+    }
+    
+    func addCard() {
+        let trimmedPrompt = newPrompt.trimmingCharacters(in: .whitespaces)
+        let trimmedAnswer = newAnswer.trimmingCharacters(in: .whitespaces)
+        guard trimmedPrompt.isEmpty == false && trimmedAnswer.isEmpty == false else { return }
+        
+        let card = Flashcard(prompt: trimmedPrompt, answer: trimmedAnswer)
+        cards.insert(card, at: 0)
+        saveData()
+    }
+    
+    func removeCards(at offsets: IndexSet) {
+        cards.remove(atOffsets: offsets)
+        saveData()
+    }
+}
+
+struct EditCardsView_Previews: PreviewProvider {
+    static var previews: some View {
+        EditCardsView()
+            .previewLayout(.fixed(width: 896, height: 414))
+    }
+}
