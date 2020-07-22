bbe59ab2c7e2ea125003a5e1186e29076830332a
OddMagnet <mibruenen@gmail.com>
2020-07-22 15:40:20 +0200

    Fix typo

--- 079-094 Controlling UI flow/Project 18/LayoutAndGeometry/LayoutAndGeometry/Alignment.swift
+++ 079-094 Controlling UI flow/Project 18/LayoutAndGeometry/LayoutAndGeometry/Alignment.swift
@@ -31,7 +31,7 @@ struct Alignment: View {
                 ForEach(0..<10) { position in
                     Text("Number \(position)")
                         .alignmentGuide(.leading) { _ in CGFloat(position) * -10 }
-                }ç
+                }
             }
             .background(Color.red)
             .frame(width: 400, height: 400)
