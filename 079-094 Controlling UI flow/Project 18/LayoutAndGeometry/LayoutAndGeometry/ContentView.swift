1bf0d41ef6899e58b48e23016fce9a6e16e4c6b6
OddMagnet <mibruenen@gmail.com>
2020-07-22 14:39:57 +0200

    Initial commit

---
+++ 079-094 Controlling UI flow/Project 18/LayoutAndGeometry/LayoutAndGeometry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LayoutAndGeometry
+//
+//  Created by Michael Brünen on 22.07.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
