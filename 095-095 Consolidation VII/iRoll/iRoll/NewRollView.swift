c92ddca0d3c5b9a39e17cbcc66d66dc56787d631
OddMagnet <mibruenen@gmail.com>
2020-07-25 13:04:10 +0200

    Add NewRollView

---
+++ 095-095 Consolidation VII/iRoll/iRoll/NewRollView.swift
@@ -0,0 +1,74 @@
+//
+//  NewRollView.swift
+//  iRoll
+//
+//  Created by Michael Brünen on 25.07.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+struct NewRollView: View {
+    @EnvironmentObject var settings: Settings
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: DiceRoll.entity(), sortDescriptors: []) var diceRolls: FetchedResults<DiceRoll>
+    @State private var results = [Int](repeating: 0, count: Settings.maxDiceAmount)
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                HStack {
+                    Text("Amount: \(settings.diceAmount)")
+                    Spacer()
+                    Text("Sides: \(settings.diceSides)")
+                }
+                .padding()
+                       
+                Text("Total: \(results.reduce(0, +))")
+                    .font(.title)
+                
+                List {
+                    ForEach(0 ..< settings.diceAmount) { dice in
+                        Text("Dice #\(dice): \(self.results[dice])")
+                    }
+                }
+                
+                Button("   Roll   ") {
+                    self.rollDice()
+                }
+                .foregroundColor(.white)
+                .padding()
+                .background(Color.blue)
+                .clipShape(Capsule())
+                .padding(.bottom)
+            }
+            .navigationBarTitle("New Roll")
+        }
+        .onAppear {
+            self.results = [Int](repeating: 0, count: self.settings.diceAmount)
+        }
+    }
+    
+    func rollDice() {
+        for dice in 0 ..< settings.diceAmount {
+            results[dice] = Int.random(in: 1...settings.diceSides)
+        }
+        let newRoll = DiceRoll(context: self.moc)
+        newRoll.rolls = results
+        do {
+            try self.moc.save()
+        } catch {
+            print("Error saving new roll")
+            print(error.localizedDescription)
+        }
+    }
+}
+
+struct NewRollView_Previews: PreviewProvider {
+    @ObservedObject static var settings = Settings()
+    static var previews: some View {
+        settings.diceAmount = 6
+        settings.diceSides = 12
+        return NewRollView().environmentObject(settings)
+    }
+}
