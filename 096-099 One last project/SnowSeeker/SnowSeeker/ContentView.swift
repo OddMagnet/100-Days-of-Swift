d8fbe4810395a357afb48e6aad91114af4fc7ea7
OddMagnet <mibruenen@gmail.com>
2020-07-26 15:08:29 +0200

    Reset

--- 096-099 One last project/SnowSeeker/SnowSeeker/ContentView.swift
+++ 096-099 One last project/SnowSeeker/SnowSeeker/ContentView.swift
@@ -8,40 +8,9 @@
 
 import SwiftUI
 
-struct User: Identifiable {
-    var id = "OddMagnet"
-    var name = "Michael Brünen"
-    var country = "Germany"
-    var pets = ["Motty, Muckel"]
-}
-
-struct UserView: View {
-    var user: User
-    
-    var body: some View {
-        Group {
-            Text("Name: \(user.name)")
-            Text("Country: \(user.country)")
-            Text("Pets: \(user.pets.joined(separator: ", "))")
-        }
-    }
-}
-
 struct ContentView: View {
-    @Environment(\.horizontalSizeClass) var sizeClass
-
     var body: some View {
-        Group {
-            if sizeClass == .compact {
-                VStack {
-                    UserView(user: User())
-                }
-            } else {
-                HStack {
-                    UserView(user: User())
-                }
-            }
-        }
+        Text("Hello, World!")
     }
 }
 
