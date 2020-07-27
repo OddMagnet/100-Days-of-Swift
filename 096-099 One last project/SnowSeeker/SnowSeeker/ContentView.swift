b95516ee543ec0450f35d74850a4fa4610a72ccb
OddMagnet <mibruenen@gmail.com>
2020-07-27 17:20:13 +0200

    Add FilterView and filtering option

--- 096-099 One last project/SnowSeeker/SnowSeeker/ContentView.swift
+++ 096-099 One last project/SnowSeeker/SnowSeeker/ContentView.swift
@@ -10,28 +10,61 @@ import SwiftUI
 
 struct ContentView: View {
     @ObservedObject var favorites = Favorites()
+    @State private var isShowingSortView = false
+    @State private var isShowingFilterView = true
+    @State private var countryFilter: Resort.CountryTypes = .all
+    @State private var sizeFilter: Resort.SizeTypes = .all
+    @State private var priceFilter: Resort.PriceTypes = .all
     let resorts: [Resort] = Bundle.main.decode("resorts.json")
+    
+    var filteredResorts: [Resort] {
+        var filtered: [Resort] = resorts
+        
+        if countryFilter != .all {
+            filtered = filtered.filter({ $0.country == countryFilter.rawValue})
+        }
+        
+        if sizeFilter != .all {
+            filtered = filtered.filter({ $0.size == sizeFilter.rawValue})
+        }
+        
+        if priceFilter != .all {
+            filtered = filtered.filter({ $0.price == priceFilter.rawValue})
+        }
+        
+        return filtered
+    }
     
     var body: some View {
         NavigationView {
-            List(resorts) { resort in
-                NavigationLink(destination: ResortView(resort: resort)) {
-                    Image(resort.country)
-                        .resortImage(width: 40, height: 25, cornerRadius: 5)
+            List {
+                if self.isShowingSortView {
                     
-                    VStack(alignment: .leading) {
-                        Text(resort.name)
-                            .font(.headline)
-                        Text("\(resort.runs) runs")
-                            .foregroundColor(.secondary)
-                    }
-                    .layoutPriority(1)
-                    
-                    if self.favorites.contains(resort) {
-                        Spacer()
-                        Image(systemName: "heart.fill")
-                            .accessibility(label: Text("This is a favorite resort"))
-                            .foregroundColor(.red)
+                }
+                if self.isShowingFilterView {
+                    FilterView(countryFilter: $countryFilter,
+                               sizeFilter: $sizeFilter,
+                               priceFilter: $priceFilter)
+                }
+                ForEach(filteredResorts) { resort in
+                    NavigationLink(destination: ResortView(resort: resort)) {
+                        Image(resort.country)
+                            .resortImage(width: 40, height: 25, cornerRadius: 5)
+                        
+                        VStack(alignment: .leading) {
+                            Text(resort.name)
+                                .font(.headline)
+                            Text("\(resort.runs) runs")
+                                .foregroundColor(.secondary)
+                        }
+                        .layoutPriority(1)
+                        
+                        if self.favorites.contains(resort) {
+                            Spacer()
+                            Image(systemName: "heart.fill")
+                                .accessibility(label: Text("This is a favorite resort"))
+                                .foregroundColor(.red)
+                        }
                     }
                 }
             }
---
+++ 096-099 One last project/SnowSeeker/SnowSeeker/FilterView.swift
@@ -0,0 +1,51 @@
+//
+//  FilterView.swift
+//  SnowSeeker
+//
+//  Created by Michael Brünen on 27.07.20.
+//  Copyright © 2020 Michael Brünen. All rights reserved.
+//
+
+import SwiftUI
+
+struct FilterView: View {
+    @Binding var countryFilter: Resort.CountryTypes
+    @Binding var sizeFilter: Resort.SizeTypes
+    @Binding var priceFilter: Resort.PriceTypes
+    
+    var body: some View {
+        Group {
+            Text("Filter by")
+                .font(.headline)
+            Picker("Country", selection: $countryFilter) {
+                ForEach(Resort.CountryTypes.allCases, id: \.self) { country in
+                    Image(country.rawValue)
+                }
+            }
+            
+            Picker("Size", selection: $sizeFilter) {
+                ForEach(Resort.SizeTypes.allCases, id: \.self) { size in
+                    Text(Resort.stringForSize(size.rawValue))
+                }
+            }
+            
+            Picker("Price", selection: $priceFilter) {
+                ForEach(Resort.PriceTypes.allCases, id: \.self) { price in
+                    Text(Resort.stringForPrice(price.rawValue))
+                }
+            }
+        }
+        .pickerStyle(SegmentedPickerStyle())
+    }
+}
+
+
+struct FilterView_Previews: PreviewProvider {
+    static var previews: some View {
+        List {
+            FilterView(countryFilter: .constant(.all),
+                       sizeFilter: .constant(.all),
+                       priceFilter: .constant(.all))
+        }
+    }
+}
